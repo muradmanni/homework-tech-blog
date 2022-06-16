69d982cf00377b5533b9d8cfceb8423b9de8dada
Murad Ali <murad.manni@gmail.com>
2022-06-17 08:53:00 +1000

    changes in gitignore

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+
+router.use('/', userRoutes);
+// router.use('/newPost', postRoutes);
+
+
+
+module.exports = router;
